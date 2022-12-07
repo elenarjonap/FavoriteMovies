72c49e39e9d3e6289c9e18f7518f5817ad77af76
Elena <elenarjona@gmail.com>
2022-12-07 23:56:22 +0100

    bootstrap

--- js/index.js
+++ js/index.js
@@ -17,12 +17,21 @@ async function fetchMoviesJson(){
         let id = movies.peliculas[index].id;
         let title = movies.peliculas[index].titulo;
         let director = movies.peliculas[index].director;
-        let genre = movies.peliculas[index].clasificacion;
+        let genre = movies.peliculas[index].genero;
+        let synopsis = movies.peliculas[index].sinopsis;
         let year = movies.peliculas[index].año;
         let img = movies.peliculas[index].caratula; 
         
         moviesSection.innerHTML += `
-        ${title}
+        <div class="card" style="width: 18rem;">
+          <img src="${img}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h3 class="card-title">${id}. ${title}</h3>
+            <h4 class="card-year">${year}</h4>
+            <h5 class="card-title">Director: ${director}</h5>
+            <h5 class="card-title">Genero: ${genre}</h5>
+          </div>
+        </div>
         `
       };
   })
